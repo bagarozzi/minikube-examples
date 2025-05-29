f6cd2b7c80bebad5fe7d9a15a0d49c5624e4a922
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-05-29 17:02:56 +0200

    Added delete statement

--- minikube/multiple-hpa-services/cleanup.sh
+++ minikube/multiple-hpa-services/cleanup.sh
@@ -2,3 +2,4 @@ kubectl delete deployment dogs-deployment cats-deployment
 kubectl delete svc cats-service dogs-service
 kubectl delete ingress nginx
 kubectl delete hpa dogs-hpa cats-hpa
+kubectl delete pod load-generator # if the load generator is still around
