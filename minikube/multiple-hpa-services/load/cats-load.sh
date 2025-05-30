fb5942e9a8292fa07f00574cd842acd920de241f
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-05-30 16:02:36 +0200

    Changed name

--- minikube/multiple-hpa-services/load/cats-load.sh
+++ minikube/multiple-hpa-services/load/cats-load.sh
@@ -1,1 +1,1 @@
-kubectl run -i --tty load-generator --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://cats-service; done"
+kubectl run -i --tty load-generator-cats --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://cats-service; done"
